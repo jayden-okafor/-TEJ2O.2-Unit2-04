baad06a9921be76675124fb78fbdafd24d8449f2
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-25 13:19:29 -0500

    removed semicolon and changed code

--- main.ts
+++ main.ts
@@ -3,16 +3,15 @@
  * Created by: Jayden
  * Created on: Feb 2026
  * This program tells you the temperature using the microbit
-*/
+ */
 
-basic.clearScreen();
+basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 let temperatureInCelcius: number
 
 input.onButtonPressed(Button.A, function() {
     temperatureInCelcius = input.temperature()
+    basic.clearScreen()
     basic.showString('The temperature is: ' + temperatureInCelcius.toString() + ' C')
 })
-
-basic.clearScreen();
